5bb243b826c6af0607a38d20907f6160d1615399
prasadalle <alle.reddy@gmail.com>
2020-08-26 12:01:44 -0700

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -1,18 +1,18 @@
 terraform {
   backend "gcs" {
-    bucket = "tf-state-gcp-batch-ingestion"
-    region = "australia-southeast1-a"
+    bucket = "fs-tf-state-gcp-batch-ingestion"
+    region = "us-west2"
     prefix = "terraform/state"
   }
 }
 
 provider "google" {
-  project = "grey-sort-challenge"
-  region = "australia-southeast1-a"
+  project = "certs-283920"
+  region = "us-west2"
 }
 
 resource "google_storage_bucket" "funky-bucket" {
   name = "batch-pipeline"
   storage_class = "REGIONAL"
-  location  = "australia-southeast1"
+  location  = "us-west2"
 }
